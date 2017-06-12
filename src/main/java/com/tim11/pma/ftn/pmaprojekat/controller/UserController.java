cbb7e50cb8a06e078bfda55d4e6a10b21ec14274
banecogic <bane93@gmail.com>
2017-06-12 02:10:03 +0200

    User service and controller introduced

--- src/main/java/com/tim11/pma/ftn/pmaprojekat/controller/UserController.java
+++ src/main/java/com/tim11/pma/ftn/pmaprojekat/controller/UserController.java
@@ -1,8 +1,25 @@
 package com.tim11.pma.ftn.pmaprojekat.controller;
 
-/**
- * Created by ChogaraTheStrongest on 5/22/2017.
- */
+import com.tim11.pma.ftn.pmaprojekat.model.User;
+import com.tim11.pma.ftn.pmaprojekat.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("user")
 public class UserController {
 
+	@Autowired
+	UserService userService;
+
+	@PostMapping
+	public User createUser(@RequestBody User user){
+		User newUser = userService.save(user);
+		return newUser;
+	}
+
+	@RequestMapping(path = "/fbProfileId/{fbProfileId}", method = RequestMethod.GET)
+	public User getUserByProfileId(@PathVariable String fbProfileId) {
+		return userService.findUserByFbProfileId(fbProfileId);
+	}
 }
